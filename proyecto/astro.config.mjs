bce2935ef9076d5faeacf42d39e72ac84859a44d
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-11-27 16:07:04 +0100

    Built 1

--- proyecto/astro.config.mjs
+++ proyecto/astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://elenalf.github.io',
+  base: '/PortFolio_Personal',
   integrations: [tailwind()]
 });
